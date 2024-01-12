6eaf436a64c85170d50090b4e53fd032ed143d1d
Aleh Mikhalchuk <OlegMikhalchuk@coherentsolutions.com>
2024-01-12 01:08:34 +0100

    end of section 3 - creation a search microservice

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,35 @@
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Lifetime.ApplicationStarted.Register(async () => 
+{
+	try
+	{
+		await DbInitializer.InitDb(app);
+	}
+	catch (Exception ex)
+	{
+		Console.WriteLine(ex);
+	}
+});
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+	=> HttpPolicyExtensions
+		.HandleTransientHttpError()
+		.WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
