106162faf74071133a1d40668b07201020d4f54f
Aleh Mikhalchuk <OlegMikhalchuk@coherentsolutions.com>
2024-01-17 02:13:36 +0100

    end of section 7: dockerizing app

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -23,10 +23,16 @@ builder.Services.AddMassTransit(x =>
 	});
 	
 	x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+	
 	x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", includeNamespace: false));
 	
 	x.UsingRabbitMq((context, cfg) => 
 	{
+		cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => 
+		{
+			host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+			host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+		});
 		cfg.ConfigureEndpoints(context);
 	});
 });
